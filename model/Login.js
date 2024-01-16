f8b953dffa3ae5a512f0f2d5cf539d77f8d5c2ad
Edwin K Sunny <127194197+edweeh@users.noreply.github.com>
2024-01-16 10:49:28 -0800

    Add files via upload

---
+++ model/Login.js
@@ -0,0 +1,12 @@
+
+// const mongoose=require("mongoose")
+// let sc=mongoose.Schema;
+// const userSchema = new sc({
+//     Username: String,
+//     Password: String,
+    
+//   });
+  
+//   var loginmodel =mongoose.model("User",userSchema)
+//   module.exports =loginmodel;
+
---
+++ routes/loginRoute.js
@@ -0,0 +1,10 @@
+// const app = require('express').Router()
+// const loginmodel = require('../model/Login');
+
+// app.post('/dnew',(request,response)=>{
+//     new loginmodel(request.body).save();
+//     response.send("Record saved Sucessfully")
+// })
+
+
+// module.exports = app
